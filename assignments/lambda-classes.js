dbafe810557e031a9bad1312f444197a05a23ff3
Daniel <danielkim.s@outlook.com>
2019-08-22 15:58:42 -0700

    Finished Lambda-classes Assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -20,9 +20,76 @@ class Instructor extends Person {
     demo(subject) {
         console.log(`Today we are learning about ${subject}`);
     }
-    grade(studentObject, subject) {
-        console.log(`${studentObject.name} received a perfect score on ${subject}`); 
+    grade(student, subject) {
+        console.log(`${student.name} received a perfect score on ${subject}`); 
     }
 }​
 
+class Student extends Person {
+    constructor(stdntAttributes) {
+        super(stdntAttributes)
+        this.previousBackground = stdntAttributes.previousBackground
+        this.className = stdntAttributes.className
+        this.favSubjects = stdntAttributes.favSubjects
+    }
+    listsSubjects() {
+        this.favSubjects.forEach(subjects => {
+            console.log(`${subjects}`);
+        })
+    }
+    PRAssignment(student, subject) {
+        console.log(`${student.name} has submitted a PR for ${subject}`);
+    }
+    springChallenge(student, subject) {
+        console.log(`${student.name} has begun sprint challenge on ${subject}`);
+    }
+}
 
+class projectManager extends Instructor {
+    constructor(pmAttributes) {
+        super(pmAttributes)
+        this.gradClassName = pmAttributes.gradClassName
+        this.favInstructor = pmAttributes.favInstructor
+    }
+    standup(channel) {
+        console.log(`@${channel} standup time`)
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+const persons = new Person({
+    name: "Rick",
+    age: 28,
+    location: "California"
+})
+
+const instructors = new Instructor({
+    name: "Charlie",
+    age: 34,
+    location: "Texas",
+    favLanguage: "JavaScript",
+    specialty: "Front-End",
+    catchPhrase: "Hello"
+})
+
+const students = new Student({
+    name: "Linda",
+    location: "Nevada",
+    age: 31,
+    previousBackground: "None",
+    className: "Full-Stack Web Development",
+    favSubjects: "Web Fundamental"
+})
+
+const pManager = new projectManager({
+    name: "John",
+    location: "Arizona",
+    age: 29,
+    favLanguage: "Python",
+    specialty: "Back-End",
+    catchPhrase: "Hi",
+    gradClassName: "CS1",
+    favInstructor: "Simon"
+})
